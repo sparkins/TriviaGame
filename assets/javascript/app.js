3d368788341c3515d905af0d0546519e839d4293
Sparky <17860180+sparkins@users.noreply.github.com>
2018-10-18 10:15:47 -0700

    created function to check answer button clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,125 +1,204 @@
-   
-var questionArr = [
-    {questionNumber: 1,
-    question: "What year was the first World Cup held?",
-    answers: ["1926", "1930", "1934", "1932"],
-    correctAns: 1   
-    },
-    {questionNumber: 2,
-        question: "Where was the first World Cup held?",
-        answers: ["England", "United States of America", "Uruguay", "Italy"],
-        correctAns: 2   
-    },
-    {questionNumber: 3,
-        question: "Who is the all time leading scorer?",
-        answers: ["Pele", "Diego Maradonna", "Miroslav Klose", "Ronaldo"],
-        correctAns: 2   
-    },
-    {questionNumber: 4,
-        question: "Which nation holds the record for the most World Cups?",
-        answers: ["Brazil", "Italy", "Germany", "France"],
-        correctAns: 0   
-    },
-    {questionNumber: 5,
-        question: "Who is the only player to score a hat-trick in a World Cup final?",
-        answers: ["Jurgen Klinsmann", "Pele", "Geoff Hurst", "Paolo Rossi"],
-        correctAns: 2   
-    },
-    {questionNumber: 6,
-        question: "Which of these teams has NOT won the World Cup on home soil?",
-        answers: ["Brazil", "Italy", "England", "Uruguay"],
-        correctAns: 0   
-    },
-    {questionNumber: 7,
-        question: "Which nation has played in every world cup?",
-        answers: ["England", "Brazil", "Argentina", "Italy"],
-        correctAns: 1   
-    },
-    {questionNumber: 8,
-        question: "Which team scored 27 goals in one World Cup?",
-        answers: ["Argentina", "Germany", "Brazil", "Hungary"],
-        correctAns: 3   
-    },
-    {questionNumber: 9,
-        question: "Who is this pictured to the right?",
-        answers: ["", "", "", ""],
-        correctAns: 0   
-    },
-    {questionNumber: 10,
-        question: "Which team has scored the fastest world Cup goal?",
-        answers: ["England", "Brazil", "Netherlands", "Turkey"],
-        correctAns: 3   
-    },
-    {questionNumber: 11,
-        question: "?",
-        answers: ["", "", "", ""],
-        correctAns: 0   
-    },
-    {questionNumber: 12,
-        question: "?",
-        answers: ["", "", "", ""],
-        correctAns: 0  
+var TriviaGame = function () {
+    var questionArr = [
+        {
+            questionNumber: 1,
+            question: "What year was the first World Cup held?",
+            answers: ["1926", "1930", "1934", "1932"],
+            correctAns: 1
+        },
+        {
+            questionNumber: 2,
+            question: "Where was the first World Cup held?",
+            answers: ["England", "United States of America", "Uruguay", "Italy"],
+            correctAns: 2
+        },
+        {
+            questionNumber: 3,
+            question: "Who is the all time leading scorer?",
+            answers: ["Pele", "Diego Maradonna", "Miroslav Klose", "Ronaldo"],
+            correctAns: 2
+        },
+        {
+            questionNumber: 4,
+            question: "Which nation holds the record for the most World Cups?",
+            answers: ["Brazil", "Italy", "Germany", "France"],
+            correctAns: 0
+        },
+        {
+            questionNumber: 5,
+            question: "Who is the only player to score a hat-trick in a World Cup final?",
+            answers: ["Jurgen Klinsmann", "Pele", "Geoff Hurst", "Paolo Rossi"],
+            correctAns: 2
+        },
+        {
+            questionNumber: 6,
+            question: "Which of these teams has NOT won the World Cup on home soil?",
+            answers: ["Brazil", "Italy", "England", "Uruguay"],
+            correctAns: 0
+        },
+        {
+            questionNumber: 7,
+            question: "Which nation has played in every world cup?",
+            answers: ["England", "Brazil", "Argentina", "Italy"],
+            correctAns: 1
+        },
+        {
+            questionNumber: 8,
+            question: "Which team scored 27 goals in one World Cup?",
+            answers: ["Argentina", "Germany", "Brazil", "Hungary"],
+            correctAns: 3
+        },
+        {
+            questionNumber: 9,
+            question: "Who is this pictured to the right?",
+            answers: ["", "", "", ""],
+            correctAns: 0
+        },
+        {
+            questionNumber: 10,
+            question: "Which team has scored the fastest world Cup goal?",
+            answers: ["England", "Brazil", "Netherlands", "Turkey"],
+            correctAns: 3
+        },
+        {
+            questionNumber: 11,
+            question: "?",
+            answers: ["", "", "", ""],
+            correctAns: 0
+        },
+        {
+            questionNumber: 12,
+            question: "This is Question 12?",
+            answers: ["A", "B", "C", "D"],
+            correctAns: 0
+        }
+    ]
+    var numCorrectAns = 0;
+    var numWrongAns = 0;
+    var questionNum = 0;
+    var question = "";
+    var answers = [];
+    var answerSelected = null;
+    var correctAns = "";
+    var qnum = 0;
+    var result = null;
+
+    // *** Function to present the start quiz screen and reset results ***
+    this.startGame = function () {
+        var self = this;
+        // console.log("Initiated StartGame function");
+        this.numCorrectAns = 0;
+        this.numWrongAns = 0;
+        this.totalAns = 0;
+        this.percentCorrect = (this.correctAns / this.totalAns) * 100;
+
+        $(".answer-button").hide();
+        $(".askQuestion").hide();
+        $(".questionNumber").hide();
+
+        // console.log("Correct Answers: " + this.numCorrectAns);
+        // console.log("Wrong Answers: " + this.numWrongAns);
+        // console.log("Questions Answered: " + this.totalAns);
+        // console.log("Percentage correct: " + this.percentCorrect);
+
+        // *** Clicking the start-button runs the function to ask the first question ***
+        $(".start-button").on("click", function () {
+
+            self.askAQuestion(qnum, questionArr);
+
+        });
     }
-]
 
+    // *** Function to build and display each question and build answer buttons ***
+    this.askAQuestion = function (qnum, questionList) {
 
-// displayQuestion ();
-// //A function to create the quiz
-// function createQuiz () {
+        // console.log("askAQuestion Function initiated");
+        var self = this;
+        // *** Determine what elements need to be visible and which should be hidden ***
+        $(".start-button").hide();
+        $(".answer-button").show();
+        $(".askQuestion").show();
+        $(".questionNumber").show();
 
+        // *** the load variables with the details from the current question ***
+        this.questionNum = questionList[qnum].questionNumber;
+        this.question = questionList[qnum].question;
+        this.answers = questionList[qnum].answers;
+        this.correctAns = questionList[qnum].correctAns;
 
-// }
+        // *** Display the question to the player ***
+        $(".askQuestion").html(this.question);
 
-this.startGame = function () {
-    console.log("Initiated StartGame function");
-    this.correctAns = 0;
-    this.wrongAns = 0;
-    this.totalAns = 0;
-    this.percentCorrect = (correctAns/totalAns)*100;
+        // *** Dynamically create and display the answers in the form of buttons *** 
+        for (var numAnsw = 0; numAnsw < this.answers.length; numAnsw++) {
+            // $("#ans" + j).html(this.answers[numAnsw]);
+            var ansBtn = $("<button>");
+            // Adding a class
+            ansBtn.addClass("btn btn-block btn-md btn-primary answer-button");
+            // Added a data-attribute
+            ansBtn.attr("data-answer", this.answers[numAnsw]);
+            // Provided the initial button text
+            ansBtn.text(this.answers[numAnsw]);
+            // Added the button to the HTML
+            $("#button-row").append(ansBtn);
+        }
 
-    console.log("Correct Answers: " + this.correctAns);
-    console.log("Wrong Answers: " + this.wrongAns);
-    console.log("Questions Answered: " + this.totalAns);
-    console.log("Percentage correct: " + this.percentCorrect);
+        $(".answer-button").on("click", function () {
+            console.log("clicked the button");
+            console.log(self.answers.length);
 
+            self.buttonClicked(answerSelected);
 
-    // This function creates a random number between 19-120 inclusive, to be assigned to the games Target Score
-
+            if (self.answerSelected === self.correctAns) {
+                $("<h2>").text("CONGRATULATIONS, that is the correct answer!");
+                console.log("woohoo");
+                result = true;
+                numCorrectAns++;
+            }
+            else {
+                $("<h2>").text("I'm sorry that is NOT the correct answer!");
+                console.log("foobar");
+                result = false;
+                numWrongAns++
+            }
+        })
+        qnum++
+        console.log("New Question Number: " + qnum);
+        this.displayAnswer(result);
     }
-//A function to show the questions
-function displayQuestion () {
-
-    // this.randomizeQuestions = function () {
-    //     console.log("Initiated randomizeQuestions function ");
-    //     this.nextRandomQuestion = [Math.floor(Math.random() * 12) + 1];
-    //     console.log("Target Score: " + this.nextRandomQuestion);
-    //     $(".targetScore").html(this.targetNumber);
-    //     return (this.nextRandomQuestion);
-
-    for (i=0; i < questionArr.length; i++) {
-        var questionNum = questionArr.questionNumber;
-        console.log("Q"+questionNum);
-        var question = questionArr.question[i];
-        console.log("Question: "+question);
-        var answers = questionArr.answers[i];
-        console.log("Answers: "+answers);
-        var correctAns = questionArr.correctAns[i];
-        console.log("Correct Answer: "+correctAns);
-        $(".question").html(question);
-        $(".answer").html(answers)
+    this.buttonClicked = function () {
+        var self = this;
+        for (var answ = 0; answ < self.answers.length; answ++) {
+            console.log(this)
+            console.log($(this));
+            console.log($(this).is('#ans' + answ));
+            if ($(this).is('#ans' + answ)) {
+                answerSelected = answ;
+                console.log("You selected: " + answerSelected + "Answer: " + self.correctAns);
+                return (answerSelected);
+            }
+        }
     }
+
+    this.displayAnswer = function (answerResult) {
+        this.answerResult = answerResult;
+        console.log("displayAnswer function initiated");
+        $('<div class="container showAnswer"></div>');
+        if (this.answerResult === true) {
+            $(".showAnswer").html("Good Job, the answer is correct!!");
+            console.log("Good Job");
+        }
+        else {
+            $(".showAnswer").html("I'm afraid that answer is incorrect!!");
+            console.log("Wawaaaahhh");
+
+        }
+        }
 
 }
 
 $(document).ready(function () {
-    var game = new triviaGame();
+    var game = new TriviaGame();
     game.startGame();
-    game.displayQuestion();
 })
-// function displayAnswer () {
 
-// }
-
-// function displayFinalResults () {
-
-// }
